a5ef3f7f654fd6995720b0d5494be327e4de93ae
Henri Bergius <henri.bergius@iki.fi>
2016-01-06 21:30:58 +0100

    Make linter happy

--- src/index.js
+++ src/index.js
@@ -1,42 +1,42 @@
-import * as CodeMirror from 'codemirror';
-import {encodeHTML,decodeHTML} from 'entities';
-import 'codemirror/lib/codemirror.css';
-import 'codemirror/theme/monokai.css';
+import * as CodeMirror from 'codemirror'
+import {encodeHTML,decodeHTML} from 'entities'
+import 'codemirror/lib/codemirror.css'
+import 'codemirror/theme/monokai.css'
 
 export default class CEd {
   constructor (options) {
-    if (!options) options = {};
-    if (!options.container) options.container = document.getElementsByTagName('textarea')[0];
-    this.container = options.container;
-    this.block = null;
-    this.onchanged = function () {};
+    if (!options) options = {}
+    if (!options.container) options.container = document.getElementsByTagName('textarea')[0]
+    this.container = options.container
+    this.block = null
+    this.onchanged = function () {}
   }
 
   init () {
     this.editor = CodeMirror.fromTextArea(this.container, {
       lineNumbers: true
-    });
+    })
     this.editor.on('changes', function () {
-      let html = this.prepareHTML(this.editor.getValue());
-      if (html === this.block.html) return;
-      this.onchanged(this.content);
-    }.bind(this));
+      let html = this.prepareHTML(this.editor.getValue())
+      if (html === this.block.html) return
+      this.onchanged(this.content)
+    }.bind(this))
   }
 
   prepareHTML (code) {
-    return '<pre>' + encodeHTML(code) + '</pre>';
+    return '<pre>' + encodeHTML(code) + '</pre>'
   }
 
   set content (block) {
-    this.block = block;
-    let el = document.createElement('div');
-    el.innerHTML = this.block.html;
-    this.editor.setValue(decodeHTML(el.textContent));
+    this.block = block
+    let el = document.createElement('div')
+    el.innerHTML = this.block.html
+    this.editor.setValue(decodeHTML(el.textContent))
   }
 
   get content () {
-    this.block.text = this.editor.getValue();
-    this.block.html = this.prepareHTML(this.block.text);
-    return this.block;
+    this.block.text = this.editor.getValue()
+    this.block.html = this.prepareHTML(this.block.text)
+    return this.block
   }
-};
+}
--- src/mount.js
+++ src/mount.js
@@ -1,31 +1,31 @@
-import CEd from '../src/index';
+import CEd from '../src/index'
 
 function setup () {
-  let ced = new CEd();
-  ced.init();
-  return ced;
+  let ced = new CEd()
+  ced.init()
+  return ced
 }
 
 function send (topic, payload) {
   window.parent.postMessage({
     topic: topic,
     payload: payload
-  }, '*');
+  }, '*')
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-  let ced = setup();
+  let ced = setup()
   ced.onchanged = function (block) {
-    send('changed', block);
-  };
+    send('changed', block)
+  }
   window.addEventListener('message', function (message) {
     switch (message.data.topic) {
       case 'setblock':
-        ced.content = message.data.payload;
-        break;
+        ced.content = message.data.payload
+        break
       case 'getblock':
-        send('setblock', ced.content);
-        break;
+        send('setblock', ced.content)
+        break
     }
-  });
-});
+  })
+})
