93bd23d18f19ffeecb5f5d0d448d15605ab9ca85
Henri Bergius <henri.bergius@iki.fi>
2016-01-07 16:34:44 +0100

    Support a 'focus' message

--- src/mount.js
+++ src/mount.js
@@ -26,6 +26,9 @@ document.addEventListener('DOMContentLoaded', function () {
       case 'getblock':
         send('setblock', ced.content)
         break
+      case 'focus':
+        ced.editor.focus()
+        break
     }
   })
 })
